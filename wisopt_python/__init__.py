cb9c5cd32c01b3b8180ab6ac051478df9d824ac7
Anmol Chawla <anmolchawla10@gmail.com>
2019-02-03 18:47:19 +0530

    Transition to flask-restplus

--- wisopt_python/__init__.py
+++ wisopt_python/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask, Blueprint
-from flask_restful import Api
+from flask_restplus import Api
 api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+api = Api(api_bp, version='1.0', title='Wisopt_Python', description='All the python APIs under one roof')
 app = Flask(__name__, instance_relative_config=True)
+portfolio_apis = api.namespace('portfolio', description='APIs dealing with the portfolio of a user')
 
 
 def create_app():
