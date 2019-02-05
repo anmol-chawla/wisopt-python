1ddb57ebb9083e790e972c927a5277314a141690
Anmol Chawla <anmolchawla10@gmail.com>
2019-02-05 14:26:03 +0530

    Change the route for the swagger ui documentation

--- wisopt_python/__init__.py
+++ wisopt_python/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restplus import Api
 api_bp = Blueprint('api', __name__)
-api = Api(api_bp, version='1.0', title='Wisopt_Python', description='All the python APIs under one roof')
+api = Api(api_bp, version='1.0', title='Wisopt_Python', description='All the python APIs under one roof', doc='/info/')
 app = Flask(__name__, instance_relative_config=True)
 portfolio_apis = api.namespace('portfolio', description='APIs dealing with the portfolio of a user')
 
