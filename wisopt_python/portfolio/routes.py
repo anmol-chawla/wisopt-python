c7917a5fee3ad1794dc6e69e0cd57f3899603133
Anmol Chawla <anmolchawla10@gmail.com>
2019-02-05 10:47:09 +0530

    Rename routes

--- wisopt_python/portfolio/routes.py
+++ wisopt_python/portfolio/routes.py
@@ -2,7 +2,7 @@ from .. import portfolio_apis as api
 from . import controller
 
 # End point declarations
-api.add_resource(controller.Education, '/Education/v1.0')
-api.add_resource(controller.Experience, '/Experience/v1.0')
-api.add_resource(controller.ExtraCurricular, '/ExtraCurricular/v1.0')
-api.add_resource(controller.Social, '/Social/v1.0')
+api.add_resource(controller.Education, '/education/v1.0')
+api.add_resource(controller.Experience, '/experience/v1.0')
+api.add_resource(controller.ExtraCurricular, '/extraCurricular/v1.0')
+api.add_resource(controller.Social, '/social/v1.0')
