16c8ddbd86d3537e605a9cee53bf047ec4d9d1f2
Siddharth <sidjain1412@gmail.com>
2019-03-03 01:32:36 +0530

    Documentation for article rating added

--- wisopt_python/discover/controller.py
+++ wisopt_python/discover/controller.py
@@ -3,7 +3,7 @@ from flask_restplus import Resource
 
 from ..common.authenticate import verify_token
 from .models.fetch import return_articles
-from .models.insertions import insert_articles
+from .models.insertions import insert_articles, rate_content
 from .. import base_parser, api
 
 
@@ -20,6 +20,13 @@ insertArticle_parser.add_argument(
     'search_term', type=str, location='args', required=True, help='The term for which articles need to be fetched')
 insertArticle_parser.add_argument(
     'task_id', type=int, location='args', required=True, help='Task ID')
+
+# Declaring a parser object for article feedback
+rateArticle_parser = base_parser.copy()
+rateArticle_parser.add_argument(
+    'content_id', type=int, location='args', required=True, help='The id for content to be rated')
+rateArticle_parser.add_argument(
+    'score', type=int, location='args', required=True, help='Score (1 or -1 or -3)')
 
 
 class Articles(Resource):
@@ -52,5 +59,20 @@ class Articles(Resource):
             task_id = request.args.get('task_id')
             insert_articles(search_term, task_id)
             return dict(status='Articles successfully inserted.'), 201
+        except Exception as e:
+            abort(400, str(e))
+
+    @api.doc(parser=rateArticle_parser)
+    @api.response(201, 'Article successfully rated.')
+    @api.response(401, 'Unauthorized access')
+    def put(self):
+        '''
+            Temporary method to rate articles and insert into database
+        '''
+        try:
+            score = request.args.get('score')
+            content_id = request.args.get('content_id')
+            rate_content(content_id, score)
+            return dict(status='Articles successfully rated.'), 201
         except Exception as e:
             abort(400, str(e))
