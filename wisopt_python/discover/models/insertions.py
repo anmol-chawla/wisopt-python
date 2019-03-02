a00e53a3e80e336f50b79449875ffe564a89c154
Siddharth <sidjain1412@gmail.com>
2019-03-03 01:34:20 +0530

    Article rating function added (temporary)

--- wisopt_python/discover/models/insertions.py
+++ wisopt_python/discover/models/insertions.py
@@ -37,3 +37,22 @@ def insert_articles(search_term, task_id):
                     (task_id, search_term, article['content_title'], article['content_description'], article['content_link'], article['content_provider'], "article", article['query']))
             con.commit()
     con.close()
+
+
+# Score an article (upvote, downvote, report wrong)
+def rate_content(content_id, score):
+    try:
+        with app.app_context():
+            con = pymysql.connect(host=current_app.config['DB_HOST'],
+                                  user=current_app.config['DB_USER'],
+                                  password=current_app.config['DB_PASSWORD'],
+                                  db=current_app.config['DB'],
+                                  charset=current_app.config['DB_CHARSET'],
+                                  cursorclass=pymysql.cursors.DictCursor,
+                                  port=current_app.config['DB_PORT'])
+        cur = con.cursor()
+    except Exception as e:
+        raise Exception('Unable to connect to server database')
+    cur.execute("UPDATE table_goals_content SET content_score = content_score + %s WHERE content_id = %s",(score, content_id))
+    con.commit()
+    con.close()
